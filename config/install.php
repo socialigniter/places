8083a77f2b124619dd8124abc9e7915bcb878737
Brennan Novak <hi@brennannovak.com>
2012-07-17 12:59:11 -0700

    moved DB values to install config

--- config/install.php
+++ config/install.php
@@ -7,7 +7,7 @@
 * Created: 		Brennan Novak
 *
 * Project:		http://social-igniter.com/
-* Source: 		http://github.com/socialigniter/app-template
+* Source: 		http://github.com/socialigniter/places
 *
 * Description: 	Install values for Places App for Social Igniter 
 */
@@ -23,3 +23,49 @@ $config['places_settings']['ratings_allow']			= 'no';
 $config['places_settings']['comments_per_page']		= '5';
 $config['places_settings']['comments_allow']		= 'no';
 $config['places_settings']['tags_display']			= 'no';
+
+
+// Places Database Table
+$config['database_messages_messages_table'] = array(
+	'place_id' => array(
+		'type' 					=> 'INT',
+		'constraint' 			=> 16,
+		'unsigned' 				=> TRUE,
+		'auto_increment'		=> TRUE
+	),
+	'content_id' => array(
+		'type' 					=> 'INT',
+		'constraint' 			=> '11',
+		'null'					=> TRUE
+	),
+	'address' => array(
+		'type'					=> 'VARCHAR',
+		'constraint'			=> 128,
+		'null'					=> TRUE
+	),
+	'district' => array(
+		'type'					=> 'VARCHAR',
+		'constraint'			=> 128,
+		'null'					=> TRUE
+	),
+	'locality' => array(
+		'type'					=> 'VARCHAR',
+		'constraint'			=> 128,
+		'null'					=> TRUE
+	),
+	'region' => array(
+		'type'					=> 'VARCHAR',
+		'constraint'			=> 128,
+		'null'					=> TRUE
+	),
+	'country' => array(
+		'type'					=> 'VARCHAR',
+		'constraint'			=> 128,
+		'null'					=> TRUE
+	),
+	'postal' => array(
+		'type'					=> 'VARCHAR',
+		'constraint'			=> 32,
+		'null'					=> TRUE
+	)										
+);
