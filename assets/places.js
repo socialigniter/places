98249198cdd1691ac8298e20f5ef5cd3875d33bf
Brennan Novak <hi@brennannovak.com>
2012-09-09 10:54:46 -0700

    started adding placeManager

--- assets/places.js
+++ assets/places.js
@@ -1,6 +1,8 @@
 /* Google Maps API Functions */
 function renderMapTile(element, address)
 {
+	console.log('heeeere: ' + element)
+
 	$(element).gmap3(
 	{
 		action : 'getLatLng',
@@ -9,6 +11,8 @@ function renderMapTile(element, address)
 		{			
 			if (result)
 			{
+				console.log(result);
+			
 				$(this).gmap3(
 				{
 					action: 'addMarker',
@@ -30,3 +34,116 @@ function renderMapTile(element, address)
 		}
 	});
 }
+
+// Add Place
+(function($)
+{
+	$.fn.placeManager = function(options)
+	{
+		var settings = {
+			action		: '',
+			module		: '',
+			type		: '',
+			title		: '',
+			parent 		: false,
+			category_id	: ''
+		};
+
+		options = $.extend({}, settings, options);
+
+		$(this).change(function()
+		{	
+			if ($(this).val() == 'add_content')
+			{
+				$.get(base_url + 'places/dialogs/add_place', function(html)
+				{
+					// Generate Dialog
+					$('<div />').html(html).dialog(
+					{
+						width	: 700,
+						title	: 'Add Place',
+						close	: function() {$(this).remove()},
+						create	: function()
+						{
+							$create_place_dialog = $(this);
+
+							renderMapTile('#place_map_map', 'Portland, OR');
+						},
+						buttons:
+						{
+							"Create" : function()
+							{								
+								// Strip Empty
+								$.validator(
+								{
+									elements : [{
+										'selector' 	: '#title', 
+										'rule'		: 'require',
+										'field'		: 'Your place needs a Title',
+										'action'	: 'label'
+									},{
+										'selector' 	: '#place_address', 
+										'rule'		: 'require',
+										'field'		: 'Your Place needs an Address',
+										'action'	: 'label'				
+									},{
+										'selector' 	: '#place_locality', 
+										'rule'		: 'require',
+										'field'		: 'Your Place needs an City',
+										'action'	: 'label'
+									},{
+										'selector' 	: '#place_region', 
+										'rule'		: 'require',
+										'field'		: 'Your Place needs an State',
+										'action'	: 'label'
+									},{
+										'selector' 	: '#place_postal', 
+										'rule'		: 'require',
+										'field'		: 'Your Place needs an Postal Code',
+										'action'	: 'label'
+									}],
+									message	 : '',
+									success	 : function()
+									{						
+										var place_data	= $('#create_place').serializeArray();
+										place_data.push({'name':'module','value':'places'},{'name':'type','value':'place'},{'name':'source','value':'website'});
+													
+										$.oauthAjax(
+										{
+											oauth 		: user_data,
+											url			: base_url + 'api/places/create',
+											type		: 'POST',
+											dataType	: 'json',
+											data		: place_data,
+											success		: function(result)
+											{									
+												if (result.status == 'success')
+												{											
+													// Makes Places Trigger Proper
+													$('#location_id').append('<option value="' + result.data.content_id + '">' + result.data.title + '</option>');												
+													$('#location_id').val(result.data.content_id);
+				
+													$create_place_dialog.dialog('close');
+													$create_place_dialog.remove();
+												}
+												else
+												{
+													alert(result.message);
+												}
+											}
+										});
+									}
+								});
+							},
+							"Cancel" : function()
+							{
+								$create_place_dialog.dialog('close');
+								$create_place_dialog.remove();							
+							}
+						}
+					});
+				});
+			}
+		});
+	};
+}) (jQuery);
--- views/dialogs/add_place.php
+++ views/dialogs/add_place.php
@@ -2,14 +2,23 @@
 	
 	<div id="place_address">
 		<h3>Title</h3>
-		<p><input type="text" name="title" id="title" class="input_bigger" value="<?= $title ?>"></p>	
-		<h3>Address</h3>
-		<p><input type="text" name="address" id="address" class="input_bigger" value="<?= $address ?>"></p>
-		<p><input type="text" name="district" id="district" class="input_bigger" value="<?= $district ?>"></p>
 		<p>
-			<input type="text" name="locality" id="locality" class="input_small" value="<?= $locality ?>">
-			<input type="text" name="region" id="region" class="input_mini" value="<?= $region ?>">
-			<input type="text" name="postal" id="postal" class="input_small" value="<?= $postal ?>">
+			<input type="text" name="title" id="title" class="input_bigger" placeholder="234 NW 10th Ave" value="<?= $title ?>">
+			<span id="title_error"></span>
+		</p>
+		<h3>Address</h3>
+		<p>
+			<input type="text" name="address" id="address" class="input_bigger" placeholder="Apt #112" value="<?= $address ?>">
+			<span id="address_error"></span>
+		</p>
+		<p><input type="text" name="district" id="district" class="input_bigger" placeholder="The Pearl" value="<?= $district ?>"></p>
+		<p>
+			<input type="text" name="locality" id="locality" class="input_small" placeholder="Portland" value="<?= $locality ?>">		
+			<input type="text" name="region" id="region" class="input_mini" placeholder="OR" value="<?= $region ?>">
+			<input type="text" name="postal" id="postal" class="input_small" placeholder="97209" value="<?= $postal ?>">
+			<span id="locality_error"></span>
+			<span id="region_error"></span>
+			<span id="postal_error"></span>
 		</p>
 		<div id="place_country"><?php // country_dropdown('country', config_item('countries'), $country) ?></div>
 		<a href="#" id="place_map_it">Map It</a>
